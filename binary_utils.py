c4e7f54b1b85688f36e14bcbb66acc700b08091b
Shaked Zychlinski <shaked.zychlinski@doubleverify.com>
2018-02-15 18:08:04 +0200

    Initial commit

---
+++ binary_utils.py
@@ -0,0 +1,38 @@
+def int_to_binary_string(n):
+    return '{0:b}'.format(n)
+
+
+def binary_string_to_int(s):
+    return int(s,2)
+
+
+def get_single_gene_bits_num(values):
+    return len(int_to_binary_string(len(values) - 1))
+
+
+def pad_binary_string(binary_string, required_length):
+    padding_size = required_length - len(binary_string)
+    padding = ''.join(['0']*padding_size)
+    return padding + binary_string
+
+
+def flip_bit_char(bit):
+    if bit == '1':
+        return '0'
+    elif bit == '0':
+        return '1'
+    else:
+        raise ValueError("No such bit-char: {}".format(bit))
+
+
+def seq_to_binary_string(sq, values):
+    binarys = map(lambda x: int_to_binary_string(sq.index(x)), sq)
+    binary_length = get_single_gene_bits_num(values)
+    padded = list(map(lambda x: pad_binary_string(x,binary_length),binarys))
+    return ''.join(padded)
+
+
+def binary_string_to_seq(binary_string, values):
+    gene_size = get_single_gene_bits_num(values)
+    binary_genes = [binary_string[i:i+gene_size] for i in range(0,len(binary_string),gene_size)]
+    return list(map(lambda x: binary_string_to_int(x), binary_genes))
---
+++ element.py
@@ -0,0 +1,81 @@
+import math
+import random
+from binary_utils import *
+
+
+class Element:
+    _genes = list()
+    _strength = 0.0
+    _probability = 0.0
+
+    def __init__(self, genes):
+        self.set_genes(genes)
+
+    def get_strength(self): return self._strength
+    def get_probability(self): return self._probability
+    def get_genes(self): return self._genes
+
+    def set_genes(self, genes): _genes = genes
+
+    def set_strength(self, strengthFunction):
+        s = strengthFunction(self._genes)
+        if s < 0.0:
+            raise ValueError("Strength must be non-negative")
+        else:
+            self._strength = s
+            self._probability = 0.0
+
+    def strength_to_probability(self, total_strength):
+        if total_strength < 0.0:
+            raise ValueError("Combined strength of the entire population cannot be negative")
+        else:
+            if math.isinf(total_strength):
+                if math.isinf(self._strength):
+                    self._probability = 1.0
+                else:
+                    self._probability = 0.0
+            else:
+                p = self._strength/total_strength
+                if p < 0.0 or p > 1.0:
+                    raise ValueError("Encountered problematic probability {} for strength {} ad combined strength {}"
+                                     .format(p,self._strength,total_strength))
+                else:
+                    self._probability = p
+
+    def mutate(self, mutation_odds, values):
+        gene_size = get_single_gene_bits_num(values)
+        new_genes = list()
+        for g in self._genes:
+            bits = int_to_binary_string(values.index(g))
+            padded_bits = pad_binary_string(bits,gene_size)
+            mutated_bits = ''
+            for b in padded_bits:
+                r = random.random()
+                if r <= mutation_odds:
+                    b = flip_bit_char(b)
+                mutated_bits += b
+            new_index = binary_string_to_int(mutated_bits) % len(values)
+            new_genes.append(values(new_index))
+        self.set_genes(new_genes)
+
+
+    def __hash__(self):
+        return hash(''.join(self._genes))
+
+    def __eq__(self, other):
+        return self.__hash__() == hash(other)
+
+    def __lt__(self, other):
+        return self.__hash__() < hash(other)
+
+    def __le__(self, other):
+        return self.__hash__() <= hash(other)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __gt__(self, other):
+        return not self.__lt__(other)
+
+    def __ge__(self, other):
+        return not self.__le__(other)
